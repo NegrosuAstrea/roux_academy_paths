eb3446261ab17196361aa067107c3dcaf16372ff
NegrosuAstrea <69705124+NegrosuAstrea@users.noreply.github.com>
2021-11-11 03:46:28 -0600

    ROUX PATS
    PATHS

---
+++ layout/header.php
@@ -0,0 +1,21 @@
+
+
+<header>
+    <div class="branding"> <img src="/roux_academy_paths/images/ralogo_monogram.png" alt="Logo">
+      <p>Roux Academy<br>
+        Art • Media • Design</p>
+    </div>
+    <div class="description">
+      <h1>Roux Academy 2016 Art Conference</h1>
+      <p>Join over 500 hundred of the most creative and brilliant minds of art colleges all around the world for five days of lectures by world-renowned art scholars and artists, and seven days and nights of gallery exhibits featuring the best in contemporary art, including painting, sculpture, and more, in the beautiful halls of Hotel Contempo in the heart of Seattle.</p>
+    </div>
+    <nav>
+      <ol>
+        <li><a href="/roux_academy_paths/index.php">HOME</a></li>
+        <li><a href="/roux_academy_paths/artists.php">ARTISTS</a></li>
+        <li><a href="/roux_academy_paths/schedule/index.php">SCHEDULE</a></li>
+        <li><a href="/roux_academy_paths/venue.php">VENUE/TRAVEL</a></li>
+        <li><a href="/roux_academy_paths/register/index.php">REGISTER</a></li>
+      </ol>
+    </nav>
+  </header>
